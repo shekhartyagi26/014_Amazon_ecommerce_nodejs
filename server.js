4b144b9609a7f3239757caa1cce5c6c7c0426daa
excellence <excellencetechnologies08@gmail.com>
2016-12-10 00:11:40 +0530

    created a route for testing user schema. tested using postman and works for x www urlencoded but not form body

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var morgan = require('morgan');// use for console all request for user.
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var User = require('./models/user');
 
 var app = express();
 
-mongoose.connect('mongodb://ammy:<dbpassword>@ds129038.mlab.com:29038/dummy_ecommerce_amit', function(err){
+mongoose.connect('mongodb://ammy:suhag007@ds129038.mlab.com:29038/dummy_ecommerce_amit', function(err){
 	if (err) {
 		console.log(err);
 	}else{
@@ -14,15 +16,22 @@ mongoose.connect('mongodb://ammy:<dbpassword>@ds129038.mlab.com:29038/dummy_ecom
 
 // Middleware 
 app.use(morgan('dev'));// this is a way to invoking morgan object. using morgan.
+app.use(bodyParser.json());// now our express can parse json data format coming from user
+app.use(bodyParser.urlencoded({ extended: true}));// in postman this will only works for x-www-formdata-urlencoded
 
 
-app.get('/name', function(req, res){
-	var name = "amit";
-    res.json("my name is " + name);
-});
+app.post('/create-user', function(req, res){
+	var user = new User();
 
-app.get('/catname', function(req, res){
-   res.json('batman');
+	user.profile.name = req.body.name;
+	user.password = req.body.password;
+	user.email = req.body.email;
+
+	user.save(function(err){
+		if(err) next(err);
+
+		res.json('Sucessfully created a new user');
+	});
 });
 
 // *** use for post data to server that create data in db ***
