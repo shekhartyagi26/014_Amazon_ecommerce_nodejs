a31a1e42fd897f432e16ca35e290f28b826ce7c5
excellence <excellencetechnologies08@gmail.com>
2016-12-11 21:20:09 +0530

    added logout feature to project

--- routes/user.js
+++ routes/user.js
@@ -50,4 +50,9 @@ router.post('/signup', function(req, res, next){
      });
 });
 
+router.get('/logout', function(req, res, next){
+	req.logout();
+	res.redirect('/');
+});
+
 module.exports = router;
